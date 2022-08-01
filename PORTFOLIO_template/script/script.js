6fc6cedc13ba038ea19eb3a93d54b2a0fddca300
nmgvnm <nmgvnm@naver.com>
2022-08-01 15:56:23 +0900

    버튼 좌표지정 및 로고 스크롤에 따른 로고크기 조정

--- PORTFOLIO_template/script/script.js
+++ PORTFOLIO_template/script/script.js
@@ -1,5 +1,6 @@
+
+// 위로 버튼 동작
 const topBtn = document.querySelector('.top_btn');
-const skill = document.querySelector('.bout_skill_wrapper');
 
 topBtn.addEventListener('click', e => {
   e.preventDefault();
@@ -16,4 +17,53 @@ addEventListener('scroll', () => {
   } else {
     topBtn.style.opacity = 0;
   }
-})
+});
+
+
+
+// top_menu 클릭시 해당 메뉴 좌표
+const project = document.querySelector('.project');
+const about = document.querySelector('.about');
+const contact = document.querySelector('article');
+const pjBtn = document.querySelector('.project_btn');
+const abBtn = document.querySelector('.about_btn');
+const ctBtn = document.querySelector('.contact_btn');
+
+pjBtn.addEventListener('click', e => {
+  e.preventDefault();
+  scrollTo({
+    top:project.offsetTop,
+    behavior: "smooth",
+  });
+});
+abBtn.addEventListener('click', e => {
+  e.preventDefault();
+  scrollTo({
+    top:about.offsetTop,
+    behavior: "smooth",
+  });
+});
+ctBtn.addEventListener('click', e => {
+  e.preventDefault();
+  console.log(contact);
+  scrollTo({
+    top:contact.offsetTop,
+    behavior: "smooth",
+  });
+});
+
+
+// 스크롤시 로고 사이즈 작게 110px -> 70px
+const logo = document.querySelector('.logo');
+
+addEventListener('scroll', () => {
+  if (scrollY > project.offsetTop) {
+    logo.classList.add('down');
+  } else if(scrollY <= project.offsetTop) {
+    logo.classList.remove('down');
+  }
+});
+
+
+
+
