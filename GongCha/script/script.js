25edc981ccd589ecbd31dc78b791b4c65198153a
nmgvnm <nmgvnm@naver.com>
2022-08-14 20:26:15 +0900

    menu slide 1차 작업 완료

--- GongCha/script/script.js
+++ GongCha/script/script.js
@@ -1,4 +1,4 @@
-const a = 300;
+const a = 250;
 const b = document.querySelectorAll('.sa');
 
 const saFunc = function() {
@@ -13,3 +13,58 @@ const saFunc = function() {
 
 window.addEventListener('load', saFunc);
 window.addEventListener('scroll', saFunc);
+
+
+// 클릭할 때 변화
+// menu title 선택
+$('.menu_list li').on('click', function () {
+  let num = $(this).index();
+  // 메뉴 선택 bar 이동
+  $('.menu_list li #bar').animate({
+    marginTop : 65 * num + 'px'
+  }, 300)
+  // 메뉴 타이틀 한글
+  $('.menu_kor #orignal').animate({
+    marginTop : -57 * num + 'px' 
+  })
+  // 메뉴 타이틀 영어
+  $('.menu_list li').removeClass('select');
+  $(this).addClass ('select')
+
+  // 메뉴 설명 멘트
+  $('.text_inner .box').removeClass('style');
+  $('.text_inner .box').eq(num).addClass ('style')
+
+  $('.bg li').removeClass('bgColor');
+  $('.bg li').eq(num).addClass ('bgColor')
+
+  $('.img_box_wrapper .img_box').removeClass('move');
+  $('.img_box_wrapper .img_box').eq(num).addClass('move')
+})
+
+
+// 자동슬라이드
+let num = 0;
+const slide = () => {
+  num = (num + 1) % 4;
+
+  $('.menu_list li #bar').animate({
+    marginTop : 65 * num + 'px'
+  }, 300)
+  // 메뉴 타이틀 한글
+  $('.menu_kor #orignal').animate({
+    marginTop : -57 * num + 'px' 
+  })
+  // 메뉴 타이틀 영어
+  $('.menu_list li').eq(num).addClass('select').siblings().removeClass('select');
+
+  // 메뉴 설명 멘트
+  $('.text_inner .box').removeClass('style');
+  $('.text_inner .box').eq(num).addClass ('style')
+
+  // background color
+  $('.bg li').removeClass('bgColor');
+  $('.bg li').eq(num).addClass ('bgColor')
+}
+
+// let auto = setInterval(slide, 4000);
