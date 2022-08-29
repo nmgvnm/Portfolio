749bf33f795d150982b569d80919646530437f14
nmgvnm <nmgvnm@naver.com>
2022-08-29 11:48:50 +0900

    임수백수정

---
+++ IMSUBACK COMPANY/script/script.js
@@ -0,0 +1,27 @@
+/* $('.nav_wrapper .menu').on('click', () => {
+  $('.nav_wrapper .menu').toggleClass('click');
+  $('.slide_menu').toggleClass('active');
+}) */
+
+// 스크롤위치
+/* $(window).scroll(function () {
+  var height = $(document).scrollTop();
+  log(height);
+  
+});
+
+function log(str){
+$('#log').text(str);
+} */
+
+const nav = document.querySelector('.nav_wrapper');
+
+addEventListener('scroll', () => {
+  if (window.scrollY > 400) {
+    nav.classList.add('down')
+  } else if (window.scrollY) {
+    nav.classList.remove('down')
+  }
+})
+
+
