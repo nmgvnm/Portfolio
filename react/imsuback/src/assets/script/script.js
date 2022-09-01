2aeaf5f754a066b5a97ca4840ea6bf2b77ada1e9
nmgvnm <nmgvnm@naver.com>
2022-09-01 09:59:29 +0900

    빈화면 수정

--- react/imsuback/src/assets/script/script.js
+++
@@ -1,7 +0,0 @@
-$('.nav_wrapper .menu').on('click', () => {
-  $('.nav_wrapper .menu').toggleClass('click');
-  $('.slide_menu').toggleClass('active');
-})
-
-const toggle = document.querySelector('.menu');
-
