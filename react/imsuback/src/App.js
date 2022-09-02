2c582267bbd972c4a25b35eb11775dd42254d27c
nmgvnm <nmgvnm@naver.com>
2022-09-02 09:36:57 +0900

    react rebuild

--- react/imsuback/src/App.js
+++ react/imsuback/src/App.js
@@ -5,7 +5,6 @@ import About from "./components/About";
 import Article from "./components/Article";
 import Info from "./components/Info";
 import Footer from "./components/Footer";
-import KakaoMap from "./components/KakaoMap";
 
 function App() {
   return (
@@ -15,7 +14,6 @@ function App() {
     <About />
     <Article />
     <Info />
-    <KakaoMap />
     <Footer />
    </div>
   );
